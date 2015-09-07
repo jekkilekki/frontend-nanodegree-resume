74716fd7028348142de2090a2ab1d384d6f523ab
Aaron Snowberger <jekkilekki@gmail.com>
2015-09-08 07:55:15 +0900

    First edition resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,65 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ 
+ var work = [
+    {
+      "employer" : "Global Prodigy Academy",
+      "title" : "Computer Teacher",
+      "location" : "Jeonju, Korea",
+      "dates" : "2012-present",
+      "description" : ""
+    }
+    
+  ];
+  
+ var projects = [
+    {
+      "title" : "Bubble Ball Korea WordPress website",
+      "dates" : "June-July 2015",
+      "description" : "",
+      "images" : [
+        "http://www.aaronsnowberger.com", 
+        "http://www.google.com"
+        ]
+    } 
+  ];
+  
+ var bio = {
+   "name" : "Aaron Snowberger",
+   "role" : "Freelance Designer/Coder",
+   "welcomeMessage" : "Hi, nice to meet you.",
+   "contacts" : {
+     "mobile" : "",
+     "email" : "aaron.snowberger@gmail.com",
+     "github" : "jekkilekki",
+     "twitter" : "jekkilekki",
+     "location" : "Jeonju, Korea"
+   },
+   "skills" : [ "HTML", "CSS", "Java", "JavaScript", "PHP", "WordPress" ]
+ };
+ 
+ var education = {
+   "schools" : [
+    {
+      "name" : "Full Sail University",
+      "location" : "Online (WinterPark, FL)",
+      "degree" : [ "MFA Media Design" ],
+      "dates" : "April 2010-April 2011",
+      "url" : "http://www.fullsail.edu"
+    },
+    {
+      "name" : "University of Wyoming",
+      "location" : "Laramie, WY",
+      "degree" : [ "BS Computer Science" ],
+      "dates" : "2000-2006",
+      "url" : "http://www.uwyo.edu"
+    }
+  ],
+  "onlineCourse" : {
+    "title" : "JavaScript Basics",
+    "school" : "Udacity",
+    "dates" : "Sept 2015",
+    "url" : "https://www.udacity.com/course/viewer#!/c-ud804"
+  }
+  };
