2184849e5b7d0f43848f5674d07f057832298a17
Aaron Snowberger <jekkilekki@gmail.com>
2015-09-09 07:47:30 +0900

    Update resumeBuilder.js - complete course

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,7 +11,7 @@ This is empty on purpose! Your code to build the resume will go here.
       "description" : ""
     }
     
-  ];
+ ];
   
  var projects = [
     {
@@ -23,7 +23,7 @@ This is empty on purpose! Your code to build the resume will go here.
         "http://www.google.com"
         ]
     } 
-  ];
+ ];
   
  var bio = {
    "name" : "Aaron Snowberger",
@@ -62,4 +62,72 @@ This is empty on purpose! Your code to build the resume will go here.
     "dates" : "Sept 2015",
     "url" : "https://www.udacity.com/course/viewer#!/c-ud804"
   }
-  };
+ };
+  
+function inName(name) {
+ name = name.trim().split(" ");
+ name[1] = name[1].toUpperCase();
+ name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+ 
+ return name[0] + " " + name[1];
+}
+
+function displayWork() {
+  for ( job in work.jobs ) {
+   // create new div for work experience
+    $( '#workExperience' ).append( HTMLworkStart );
+    
+    // concat employer and title
+    var formattedEmployer = HTMLworkEmployer.replace( "%data%", work.jobs[job].employer );
+    var formattedTitle = HTMLworkTitle.replace( "%data%", work.jobs[job].title );
+    var formattedEmployerTitle = formattedEmployer + formattedTitle;
+    $( ".work-entry:last" ).append( formattedEmployerTitle );
+    
+    var formattedLocation = HTMLworkLocation.replace( "%data%", work.jobs[job].location );
+    $( ".work-entry:last" ).append( formattedLocation );
+    
+    var formattedDates = HTMLworkDates.replace( "%data%", work.jobs[job].dates );
+    $( ".work-entry:last" ).append( formattedDates );
+    
+    var formattedDescription = HTMLworkDescription.replace( "%data%", work.jobs[job].description );
+    $( ".work-entry:last" ).append( formattedDescription );
+  }
+};
+displayWork();
+
+projects.display = function() {
+ for ( project in projects.projects ) {
+  $("#projects").append(HTMLprojectStart);
+  
+  var formattedTitle = HTMLprojectTitle.replace( "%data%", projects.projects[project].title );
+  $(".project-entry:last").append( formattedTitle );
+  
+  var formattedDates = HTMLprojectDates.replace( "%data%", projects.projects[project].dates );
+  $(".project-entry:last").append( formattedDates );
+  
+  var formattedDescription = HTMLprojectDescription.replace( "%data%", projects.projects[project].description );
+  $(".project-entry:last").append( formattedDescription );
+  
+  if ( projects.projects[project].images.length > 0 ) {
+   for ( image in projects.projects[project].images ) {
+    var formattedImage = HTMLprojectImage.replace( "%data%", projects.projects[project].images[image] );
+    $(".project-entry:last").append( formattedImage );
+   }
+  }
+  
+ }
+}
+
+function locationizer(work_obj) {
+    var locationArray = [];
+    
+    for( var location in work_obj.jobs ) {
+        locationArray.push( work_obj.jobs[location].location );
+    }
+    
+    return locationArray;
+}
+
+$('#main').append( internationalizeButton );
+// here's a map
+$('#mapDiv').append( googleMap );
